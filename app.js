d4c14f404fef5e82075dbdd7b16bc9015359c356
Raymond <yeruiminqq@gmail.com>
2014-06-02 23:09:32 +0800

    add register and test mongo

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var MongoStore = require('connect-mongo')(express);
 var settings = require('./settings');
+var flash = require('connect-flash');
 
 var routes = require('./routes');
 
@@ -15,6 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash());
 
 app.use(favicon());
 app.use(logger('dev'));
