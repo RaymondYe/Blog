cff19c40a59cf6921fda1d2e66194a705ddaaeae
Raymond <yeruiminqq@gmail.com>
2014-06-07 02:30:37 +0800

    add comment module

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto');
 var User = require('../modules/user.js');
 var Post = require('../modules/post.js');
+var Comment = require('../modules/comment.js');
 
 //upload file
 var fs = require('fs');
@@ -239,6 +240,29 @@ module.exports = function(app) {
       }
       req.flash('success', '删除成功!');
       res.redirect('/');
+    });
+  });
+
+  //comment module
+  app.post('/u/:name/:day/:title', function(req, res) {
+    var date = new Date(),
+      time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+      date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
+    var comment = {
+      name: req.body.name,
+      email: req.body.email,
+      website: req.body.website,
+      time: time,
+      content: req.body.content
+    };
+    var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+    newComment.save(function(err) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('back');
+      }
+      req.flash('success', '留言成功!');
+      res.redirect('back');
     });
   });
 
