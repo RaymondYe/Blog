c9f63942c2052d37e793a19341af8ee031eb10a0
Raymond <yeruiminqq@gmail.com>
2014-06-08 23:37:40 +0800

    add tag page func

--- routes/index.js
+++ routes/index.js
@@ -294,6 +294,38 @@ module.exports = function(app) {
     });
   });
 
+  app.get('/tags', function(req, res) {
+    Post.getTags(function(err, posts) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('tags', {
+        title: '标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
+  app.get('/tags/:tag', function(req, res) {
+    Post.getTag(req.params.tag, function(err, posts) {
+      if (err) {
+        req.flash('error', err);
+        return res.redirect('/');
+      }
+      res.render('tag', {
+        title: 'TAG:' + req.params.tag,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+
   function checkLogin(req, res, next) {
     if (!req.session.user) {
       req.flash('error', '未登录!');
